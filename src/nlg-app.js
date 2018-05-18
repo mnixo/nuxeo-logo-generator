8f5e8bb5cff4ba82aba4994cf3d93d233e845102
Nelson Silva <nsilva@nuxeo.com>
2018-05-18 12:44:26 +0100

    move to lit-html

--- src/nlg-app.js
+++ src/nlg-app.js
@@ -1,107 +1,88 @@
-import { PolymerElement, html } from '@polymer/polymer/polymer-element';
-import '@polymer/iron-icon/iron-icon';
-import '@polymer/iron-icons/iron-icons';
-import '@polymer/paper-button/paper-button';
-import '@polymer/paper-checkbox/paper-checkbox';
-import '@polymer/paper-dropdown-menu/paper-dropdown-menu';
-import '@polymer/paper-input/paper-input';
-import '@polymer/paper-item/paper-item';
-import '@polymer/paper-listbox/paper-listbox';
-import '@polymer/paper-styles/paper-styles';
-import './nlg-color-picker';
-import './nlg-colors';
-import './nlg-size-picker';
-import './nlg-templates';
-import './svg-wrapper';
+import { LitElement, html } from '@polymer/lit-element';
+import {unsafeHTML} from 'lit-html/lib/unsafe-html.js';
+import '@polymer/iron-icon/iron-icon.js';
+import '@polymer/iron-icons/iron-icons.js';
+import '@polymer/paper-button/paper-button.js';
+import '@polymer/paper-checkbox/paper-checkbox.js';
+import '@polymer/paper-dropdown-menu/paper-dropdown-menu.js';
+import '@polymer/paper-input/paper-input.js';
+import '@polymer/paper-item/paper-item.js';
+import '@polymer/paper-listbox/paper-listbox.js';
+import '@polymer/paper-styles/paper-styles.js';
+import './nlg-color-picker.js';
+import './nlg-size-picker.js';
+import { COLORS } from './colors.js';
+import { TEMPLATES, getSkeleton } from './templates.js';
+import {renderSVG} from './svg-renderer.js';
 
-class NLGApp extends PolymerElement {
-  static get template() {
+window.devicePixelRatio = 1;
+
+class NLGApp extends LitElement {
+  _render({template, width, height, primaryFill, primaryOpacity, secondaryFill, secondaryOpacity, backgroundFill, backgroundOpacity, _showAlphaLayer}) {
+
+    const content = getSkeleton(template, {width, height, primaryFill, primaryOpacity, secondaryFill, secondaryOpacity, backgroundFill, backgroundOpacity});
+
     return html`
-      <style>
-        :host {
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-        }
-        paper-dropdown-menu.template-picker {
-          width: 18em;
-        }
-        nlg-color-picker,
-        nlg-size-picker {
-          display: flex;
-        }
-        paper-checkbox.show-alpha {
-          margin-top: 1em;
-        }
-        paper-button.download {
-          background-color: #fff;
-          margin-top: 1em;
-        }
-        svg-wrapper.hidden {
-          width: 0;
-          height: 0;
-          opacity: 0;
-        }
-        svg-wrapper.displayed {
-          display: flex;
-          width: fit-content;
-        }
-        svg-wrapper.show-alpha {
-          background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAIAAAACCAIAAAD91JpzAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH4gUQFToDqewmngAAAB1pVFh0Q29tbWVudAAAAAAAQ3JlYXRlZCB3aXRoIEdJTVBkLmUHAAAAGUlEQVQIHQEOAPH/Aebm5tzc3ALc3NwkJCRFcghK/x9NRAAAAABJRU5ErkJggg==');
-          background-size: 20px 20px;
-          image-rendering: pixelated;
-        }
-        div.svg-wrapper-container {
-          border: dashed rgba(0, 0, 0, 0.5) 1px;
-          margin: 1em 0;
-        }
-      </style>
-  
-      <paper-dropdown-menu class="template-picker" label="Logo Template" on-iron-select="_itemSelected">
-        <paper-listbox slot="dropdown-content" selected="0">
-          <template is="dom-repeat" items="[[_getTemplates()]]">
-            <paper-item id="[[item.id]]">[[item.id]]</paper-item>
-          </template>
-        </paper-listbox>
-      </paper-dropdown-menu>
-  
-      <nlg-size-picker height="{{height}}" width="{{width}}"></nlg-size-picker>
-  
-      <template is="dom-if" if="[[template.primaryFill]]">
-        <nlg-color-picker label="Primary" fill="{{primaryFill}}" opacity="{{primaryOpacity}}"></nlg-color-picker>
-      </template>
-  
-      <template is="dom-if" if="[[template.secondaryFill]]">
-        <nlg-color-picker label="Secondary" fill="{{secondaryFill}}" opacity="{{secondaryOpacity}}"></nlg-color-picker>
-      </template>
-  
-      <template is="dom-if" if="[[template.backgroundFill]]">
-        <nlg-color-picker label="Background" fill="{{backgroundFill}}" opacity="{{backgroundOpacity}}"></nlg-color-picker>
-      </template>
-  
-      <paper-checkbox class="show-alpha" checked="{{_showAlphaLayer}}">Show alpha layer in the preview</paper-checkbox>
-  
-      <paper-button class="download" raised="" on-click="_download">Download</paper-button>
-  
-      <div class="svg-wrapper-container">
-        <svg-wrapper class\$="displayed [[_getAlphaClass(_showAlphaLayer)]]" svg-data="[[content]]"></svg-wrapper>
-      </div>
-  
-      <svg-wrapper class="hidden" id="svgWrapper" svg-data="[[content]]" pre-render=""></svg-wrapper>
-      <a id="downloadLink" hidden=""></a>
-  
-      <nlg-colors id="colors"></nlg-colors>
-      <nlg-templates id="templates"></nlg-templates>
-    `;
-  }
+    <style>
+      :host {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+      }
+      paper-dropdown-menu.template-picker {
+        width: 18em;
+      }
+      nlg-color-picker,
+      nlg-size-picker {
+        display: flex;
+      }
+      paper-checkbox.show-alpha {
+        margin-top: 1em;
+      }
+      paper-button.download {
+        background-color: #fff;
+        margin-top: 1em;
+      }
 
-  static get is() {
-    return 'nlg-app';
+      .svg-wrapper {
+        display: flex;
+        width: fit-content;
+      }
+      
+      .show-alpha {
+        background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAIAAAACCAIAAAD91JpzAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH4gUQFToDqewmngAAAB1pVFh0Q29tbWVudAAAAAAAQ3JlYXRlZCB3aXRoIEdJTVBkLmUHAAAAGUlEQVQIHQEOAPH/Aebm5tzc3ALc3NwkJCRFcghK/x9NRAAAAABJRU5ErkJggg==');
+        background-size: 20px 20px;
+        image-rendering: pixelated;
+      }
+      div.svg-wrapper-container {
+        border: dashed rgba(0, 0, 0, 0.5) 1px;
+        margin: 1em 0;
+      }
+    </style>
+
+    <paper-dropdown-menu class="template-picker" label="Logo Template" on-iron-select="${(e) => this._itemSelected(e.detail.item.id)}">
+      <paper-listbox slot="dropdown-content" selected="0">
+        ${Object.keys(TEMPLATES).map((id) => html` <paper-item id="${id}">${id}</paper-item>`)}
+      </paper-listbox>
+    </paper-dropdown-menu>
+
+    <nlg-size-picker height="${height}" width="${width}" on-size-changed="${(e) => this._setSize(e.detail) }}"></nlg-size-picker>
+
+    ${primaryFill ? html`<nlg-color-picker label="Primary" fill="${primaryFill}" opacity="${primaryOpacity}" on-color-changed="${e => this._setPrimaryColor(e.detail)}"></nlg-color-picker>` : null}
+    ${secondaryFill ? html`<nlg-color-picker label="Secondary" fill="${secondaryFill}" opacity="${secondaryOpacity}" on-color-changed="${e => this._setSecondaryColor(e.detail)}"></nlg-color-picker>` : null}
+    ${backgroundFill ? html`<nlg-color-picker label="Background" fill="${backgroundFill}" opacity="${backgroundOpacity}" on-color-changed="${e => this._setBackgroundColor(e.detail)}"></nlg-color-picker>` : null}  
+
+    <paper-checkbox checked="${_showAlphaLayer}" on-change="${e => this._showAlphaLayer = e.target.checked}">Show alpha layer in the preview</paper-checkbox>
+
+    <paper-button class="download" raised on-click="${() => this._download(content)}">Download</paper-button>
+    <div class="svg-wrapper-container">
+      <div class$="${`svg-wrapper ${this._showAlphaLayer ? 'show-alpha' : ''}`}">${unsafeHTML(content)}</div>
+    </div>
+`;
   }
 
   static get properties() {
     return {
-      content: String,
       width: Number,
       height: Number,
       primaryFill: String,
@@ -110,71 +91,52 @@ class NLGApp extends PolymerElement {
       secondaryOpacity: Number,
       backgroundFill: String,
       backgroundOpacity: Number,
-      template: Object,
-      _showAlphaLayer: {
-        type: Boolean,
-        value: true,
-      },
+      template: String,
+      _showAlphaLayer: Boolean,
     };
   }
 
-  static get observers() {
-    return [
-      '_computeContent(template, width, height, primaryFill, primaryOpacity, secondaryFill, secondaryOpacity,' +
-      'backgroundFill, backgroundOpacity)',
-    ]
+  _itemSelected(id) {
+    this.template = id;
+    const template = TEMPLATES[id];
+    this.width = template.width;
+    this.height = template.height;
+    this.primaryFill = template.primaryFill;
+    this.primaryOpacity = template.primaryOpacity;
+    this.secondaryFill = template.secondaryFill;
+    this.secondaryOpacity = template.secondaryOpacity;
+    this.backgroundFill = template.backgroundFill;
+    this.backgroundOpacity = template.backgroundOpacity;
   }
 
-  ready() {
-    window.devicePixelRatio = 1;
-    super.ready();
+  _setSize({width, height}) {
+    if (width) this.width = width;
+    if (height) this.height = height;
   }
 
-  _getColors() {
-    return this.$.colors.getAll();
+  _setPrimaryColor({fill, opacity}) {
+    if (fill) this.primaryFill = fill;
+    if (opacity) this.primaryOpacity = opacity;
   }
 
-  _getTemplates() {
-    return this.$.templates.getAll();
+  _setSecondaryColor({fill, opacity}) {
+    if (fill) this.secondaryFill = fill;
+    if (opacity) this.secondaryOpacity = opacity;
   }
 
-  _computeContent() {
-    if (!this.template) {
-      return this.content = '';
-    }
-    const width = this.width ? this.width : this.template.width;
-    const height = this.height ? this.height : this.template.height;
-    const primaryFill = this.primaryFill ? this.primaryFill : this.template.primaryFill;
-    const primaryOpacity = this.primaryOpacity ? this.primaryOpacity : this.template.primaryOpacity;
-    const secondaryFill = this.secondaryFill ? this.secondaryFill : this.template.secondaryFill;
-    const secondaryOpacity = this.secondaryOpacity ? this.secondaryOpacity : this.template.secondaryOpacity;
-    const backgroundFill = this.backgroundFill ? this.backgroundFill : this.template.backgroundFill;
-    const backgroundOpacity = this.backgroundOpacity ? this.backgroundOpacity : this.template.backgroundOpacity;
-    this.content = this.$.templates.getSkeleton(width, height, this.template.viewBox, primaryFill, primaryOpacity,
-      secondaryFill, secondaryOpacity, backgroundFill, backgroundOpacity, this.template.geometry);
+  _setBackgroundColor({fill, opacity}) {
+    if (fill) this.backgroundFill = fill;
+    if (opacity) this.backgroundOpacity = opacity;
   }
 
-  _itemSelected(e) {
-    this.template = this.$.templates.get(e.detail.item.id);
-    this.width = this.template.width;
-    this.height = this.template.height;
-    this.primaryFill = this.template.primaryFill;
-    this.primaryOpacity = this.template.primaryOpacity;
-    this.secondaryFill = this.template.secondaryFill;
-    this.secondaryOpacity = this.template.secondaryOpacity;
-    this.backgroundFill = this.template.backgroundFill;
-    this.backgroundOpacity = this.template.backgroundOpacity;
-  }
-
-  _download() {
-    const link = this.$.downloadLink;
-    link.download = `${this.template.id}-${this.height}x${this.width}`;
-    link.href = this.$.svgWrapper.shadowRoot.querySelector('canvas').toDataURL("image/png");
-    link.click();
-  }
-
-  _getAlphaClass() {
-    return this._showAlphaLayer ? 'show-alpha' : '';
+  _download(svg) {
+    renderSVG(svg).then((url) => {
+      const link = document.createElement("a");
+      link.setAttribute('hidden', true);
+      link.download = `${this.template}-${this.height}x${this.width}`;
+      link.href = url;
+      link.click();
+    });
   }
 }
-window.customElements.define(NLGApp.is, NLGApp);
+window.customElements.define('nlg-app', NLGApp);
---
+++ src/templates.js
@@ -0,0 +1,172 @@
+export const TEMPLATES = {
+  'Avatar-01': {
+    'viewBox': `0 0 225 225`,
+    'width': 250,
+    'height': 250,
+    'primaryFill': '#0066FF',
+    'primaryOpacity': 1.0,
+    'backgroundFill': '#FFFFFF',
+    'backgroundOpacity': 0.0,
+    'geometry': `
+      <polygon class="primary" points="32.25 32.25 32.25 64.85 79.9 112.5 32.25 160.15 32.25 192.75 64.85 192.75 112.5 145.1 160.15 192.75 192.75 192.75 192.75 160.15 145.1 112.5 192.75 64.85 192.75 32.25 160.15 32.25 112.5 79.9 64.85 32.25 32.25 32.25"/>
+    `,
+  },
+  'Avatar-02': {
+    'viewBox': `0 0 16 16`,
+    'width': 250,
+    'height': 250,
+    'primaryFill': '#FFFFFF',
+    'primaryOpacity': 1.0,
+    'secondaryFill': '#1F28BF',
+    'secondaryOpacity': 1.0,
+    'backgroundFill': '#FFFFFF',
+    'backgroundOpacity': 0.0,
+    'geometry': `
+      <defs>
+        <mask id="mask">
+          <rect fill="white" width="100%" height="100%"/>
+          <polygon fill="black" points="2.32 2.33 2.32 4.64 5.69 8.01 2.32 11.38 2.32 13.68 4.63 13.68 8 10.31 11.37 13.68 13.68 13.68 13.68 11.38 10.31 8.01 13.68 4.64 13.68 2.33 11.37 2.33 8 5.7 4.63 2.33 2.32 2.33"/>
+        </mask>
+      </defs>
+      <rect class="secondary" width="100%" height="100%" rx="1" ry="1" mask="url(#mask)"/>
+      <polygon class="primary" points="2.32 2.33 2.32 4.64 5.69 8.01 2.32 11.38 2.32 13.68 4.63 13.68 8 10.31 11.37 13.68 13.68 13.68 13.68 11.38 10.31 8.01 13.68 4.64 13.68 2.33 11.37 2.33 8 5.7 4.63 2.33 2.32 2.33"/>
+    `,
+  },
+  'Avatar-03': {
+    'viewBox': `0 0 16 16`,
+    'width': 250,
+    'height': 250,
+    'primaryFill': '#0066FF',
+    'primaryOpacity': 1.0,
+    'secondaryFill': '#73D2CF',
+    'secondaryOpacity': 1.0,
+    'backgroundFill': '#FFFFFF',
+    'backgroundOpacity': 0.0,
+    'geometry': `
+      <defs>
+        <mask id="mask">
+          <rect fill="white" width="100%" height="100%"/>
+          <polygon fill="black" points="2.98 2.98 2.98 5.02 5.96 8 2.98 10.99 2.98 13.03 5.02 13.03 8 10.04 10.98 13.03 13.02 13.03 13.02 10.99 10.04 8 13.02 5.02 13.02 2.98 10.98 2.98 8 5.96 5.02 2.98 2.98 2.98"/>
+        </mask>
+      </defs>
+      <rect class="secondary" width="100%" height="100%" rx="8" ry="8" mask="url(#mask)"/>
+      <polygon class="primary" points="2.98 2.98 2.98 5.02 5.96 8 2.98 10.99 2.98 13.03 5.02 13.03 8 10.04 10.98 13.03 13.02 13.03 13.02 10.99 10.04 8 13.02 5.02 13.02 2.98 10.98 2.98 8 5.96 5.02 2.98 2.98 2.98"/>
+    `,
+  },
+  'NUXEO-LOGO-1': {
+    'viewBox': `0 0 362.98 68.06`,
+    'width': 533,
+    'height': 100,
+    'primaryFill': '#1F28BF',
+    'primaryOpacity': 1.0,
+    'secondaryFill': '#0066FF',
+    'secondaryOpacity': 1.0,
+    'backgroundFill': '#FFFFFF',
+    'backgroundOpacity': 0.0,
+    'geometry': `
+      <polygon class="primary" points="147.46 0 147.46 13.82 167.67 34.03 147.46 54.23 147.46 68.06 161.29 68.06 181.49 47.85 201.7 68.06 215.52 68.06 215.52 54.23 195.32 34.03 215.52 13.82 215.52 0 201.7 0 181.49 20.2 161.29 0 147.46 0"/>
+      <polygon class="secondary" points="124.78 0 124.78 51.75 90.75 51.75 90.75 0 73.73 0 73.73 51.05 90.75 68.06 141.79 68.06 141.79 0 124.78 0"/>
+      <polygon class="secondary" points="51.05 0 0 0 0 68.06 17.02 68.06 17.02 16.31 51.05 16.31 51.05 68.06 68.06 68.06 68.06 17.02 51.05 0"/>
+      <path class="secondary" d="M346,0h-51V51l17,17h51V17Zm0,51.75h-34V16.31h34Z"/>
+      <polygon class="secondary" points="289.25 16.31 289.25 0 221.19 0 221.19 68.06 289.25 68.06 289.25 51.75 238.21 51.75 238.21 41.95 288.55 41.95 288.55 25.76 238.21 25.76 238.21 16.31 289.25 16.31"/>
+    `,
+  },
+  'NUXEO-LOGO-3': {
+    'viewBox': `0 0 215.52 214.28`,
+    'width': 250,
+    'height': 250,
+    'primaryFill': '#1F28BF',
+    'primaryOpacity': 1.0,
+    'secondaryFill': '#0066FF',
+    'secondaryOpacity': 1.0,
+    'backgroundFill': '#FFFFFF',
+    'backgroundOpacity': 0.0,
+    'geometry': `
+      <polygon class="primary" points="0 146.22 0 160.04 20.2 180.25 0 200.45 0 214.28 13.82 214.28 34.03 194.07 54.23 214.28 68.06 214.28 68.06 200.45 47.85 180.25 68.06 160.04 68.06 146.22 54.23 146.22 34.03 166.42 13.82 146.22 0 146.22"/>
+      <polygon class="primary" points="147.46 0 147.46 13.82 167.67 34.03 147.46 54.23 147.46 68.06 161.29 68.06 181.49 47.85 201.7 68.06 215.52 68.06 215.52 54.23 195.32 34.03 215.52 13.82 215.52 0 201.7 0 181.49 20.2 161.29 0 147.46 0"/>
+      <polygon class="secondary" points="51.05 73.26 51.05 125.02 17.02 125.02 17.02 73.26 0 73.26 0 124.31 17.02 141.32 68.06 141.32 68.06 73.26 51.05 73.26"/>
+      <polygon class="secondary" points="124.78 0 124.78 51.75 90.75 51.75 90.75 0 73.73 0 73.73 51.05 90.75 68.06 141.79 68.06 141.79 0 124.78 0"/>
+      <polygon class="secondary" points="51.05 0 0 0 0 68.06 17.02 68.06 17.02 16.31 51.05 16.31 51.05 68.06 68.06 68.06 68.06 17.02 51.05 0"/>
+      <path class="secondary" d="M198.51,146.22h-51v51l17,17h51v-51Zm0,51.75h-34V162.52h34Z"/>
+      <polygon class="secondary" points="141.79 162.52 141.79 146.22 73.73 146.22 73.73 214.28 141.79 214.28 141.79 197.97 90.75 197.97 90.75 188.16 141.08 188.16 141.08 171.97 90.75 171.97 90.75 162.52 141.79 162.52"/>
+      <polygon class="secondary" points="215.13 89.18 215.13 72.88 147.07 72.88 147.07 140.93 215.13 140.93 215.13 124.63 164.09 124.63 164.09 114.82 214.43 114.82 214.43 98.63 164.09 98.63 164.09 89.18 215.13 89.18"/>
+    `,
+  },
+  'NUXEO-LOGO-3-BL': {
+    'viewBox': `0 0 215.52 214.28`,
+    'width': 250,
+    'height': 250,
+    'primaryFill': '#1F28BF',
+    'primaryOpacity': 1.0,
+    'secondaryFill': '#0066FF',
+    'secondaryOpacity': 1.0,
+    'backgroundFill': '#FFFFFF',
+    'backgroundOpacity': 0.0,
+    'geometry': `
+      <polygon class="primary" points="0 146.22 0 160.04 20.2 180.25 0 200.45 0 214.28 13.82 214.28 34.03 194.07 54.23 214.28 68.06 214.28 68.06 200.45 47.85 180.25 68.06 160.04 68.06 146.22 54.23 146.22 34.03 166.42 13.82 146.22 0 146.22"/>
+      <polygon class="secondary" points="51.05 73.26 51.05 125.02 17.02 125.02 17.02 73.26 0 73.26 0 124.31 17.02 141.32 68.06 141.32 68.06 73.26 51.05 73.26"/>
+      <polygon class="secondary" points="51.05 0 0 0 0 68.06 17.02 68.06 17.02 16.31 51.05 16.31 51.05 68.06 68.06 68.06 68.06 17.02 51.05 0"/>
+      <path class="secondary" d="M198.51,146.22h-51v51l17,17h51v-51Zm0,51.75h-34V162.52h34Z"/>
+      <polygon class="secondary" points="141.79 162.52 141.79 146.22 73.73 146.22 73.73 214.28 141.79 214.28 141.79 197.97 90.75 197.97 90.75 188.16 141.08 188.16 141.08 171.97 90.75 171.97 90.75 162.52 141.79 162.52"/>
+    `,
+  },
+  'NUXEO-LOGO-3-TR': {
+    'viewBox': `0 0 215.52 214.28`,
+    'width': 250,
+    'height': 250,
+    'primaryFill': '#1F28BF',
+    'primaryOpacity': 1.0,
+    'secondaryFill': '#0066FF',
+    'secondaryOpacity': 1.0,
+    'backgroundFill': '#FFFFFF',
+    'backgroundOpacity': 0.0,
+    'geometry': `
+      <polygon class="primary" points="147.46 0 147.46 13.82 167.67 34.03 147.46 54.23 147.46 68.06 161.29 68.06 181.49 47.85 201.7 68.06 215.52 68.06 215.52 54.23 195.32 34.03 215.52 13.82 215.52 0 201.7 0 181.49 20.2 161.29 0 147.46 0"/>
+      <polygon class="secondary" points="124.78 0 124.78 51.75 90.75 51.75 90.75 0 73.73 0 73.73 51.05 90.75 68.06 141.79 68.06 141.79 0 124.78 0"/>
+      <polygon class="secondary" points="51.05 0 0 0 0 68.06 17.02 68.06 17.02 16.31 51.05 16.31 51.05 68.06 68.06 68.06 68.06 17.02 51.05 0"/>
+      <path class="secondary" d="M198.51,146.22h-51v51l17,17h51v-51Zm0,51.75h-34V162.52h34Z"/>
+      <polygon class="secondary" points="215.13 89.18 215.13 72.88 147.07 72.88 147.07 140.93 215.13 140.93 215.13 124.63 164.09 124.63 164.09 114.82 214.43 114.82 214.43 98.63 164.09 98.63 164.09 89.18 215.13 89.18"/>
+    `,
+  },
+  'NUXEO-X': {
+    'viewBox': `0 0 450 450`,
+    'width': 250,
+    'height': 250,
+    'primaryFill': '#0066FF',
+    'primaryOpacity': 1.0,
+    'backgroundFill': '#FFFFFF',
+    'backgroundOpacity': 0.0,
+    'geometry': `
+      <polygon class="primary" points="113.08 112.62 113.08 158.27 179.84 225.03 113.08 291.76 113.08 337.44 158.76 337.44 225.49 270.68 292.25 337.44 337.9 337.44 337.9 291.76 271.17 225.03 337.9 158.27 337.9 112.62 292.25 112.62 225.49 179.35 158.76 112.62 113.08 112.62"/>
+    `,
+  },
+};
+
+
+export const getSkeleton = (template, params) => {
+  if (!template) return ''
+  const options = Object.assign({}, TEMPLATES[template]);
+  const {width, height, viewBox, primaryFill, primaryOpacity, secondaryFill, secondaryOpacity, backgroundFill, backgroundOpacity, geometry} = Object.assign(options, params);
+  return `
+    <svg xmlns="http://www.w3.org/2000/svg" width="${width}px" height="${height}px" viewBox="${viewBox}">
+      <defs>
+        <style>
+          .primary {
+            fill: ${primaryFill};
+            opacity: ${primaryOpacity};
+          }
+          .secondary {
+            fill: ${secondaryFill};
+            opacity: ${secondaryOpacity};
+          }
+          .background {
+            fill: ${backgroundFill};
+            opacity: ${backgroundOpacity};
+          }
+        </style>
+      </defs>
+      <rect class="background" width="100%" height="100%"/>
+      ${geometry}
+    </svg>
+  `;
+};
